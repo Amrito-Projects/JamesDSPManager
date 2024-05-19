ece05a3194ae4b736ba81307b78f7baf2788d353
Alen 24 <alenbijuk2003@gmail.com>
2024-05-19 16:45:52 +0530

    [FixUP] JamesDSPManager: fix paths

--- system/app/Android.mk
+++ system/app/Android.mk
@@ -1,1 +1,10 @@
-include $(call all-subdir-makefiles)
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := JamesDSPManager
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_SRC_FILES := JamesDSPManager/JamesDSPManagerThePBone.apk
+LOCAL_OVERRIDES_PACKAGES := AudioFX
+include $(BUILD_PREBUILT)
